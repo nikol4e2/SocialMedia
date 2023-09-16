885d8d31e97cb94ce69d06e75086e6bfb671be26
Nikola Arsov <arsovnikola1@gmail.com>
2023-09-16 11:51:09 +0200

    added profile model and signup functionality"

--- App/models.py
+++ App/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
-class Profile (models.Model):
-    user = pass
-    id_user = pass
-    bio = pass
-    profile = models.ImageField(upload_to='profile_images',default='book-icon.png')
-    location = pass
+User = get_user_model()
 
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile = models.ImageField(upload_to='profile_images', default='book-icon.png')
+    location = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.username
--- App/views.py
+++ App/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+from App.models import Profile
+
 
 # Create your views here.
 
-def index (request):
+def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,"User with that email already exists!")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,"Username is already taken!")
+                return  redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to settings page
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('login')
+        else:
+            messages.info(request,"Passwords do not match!")
+
+    else:
+        return render(request, 'signup.html')
