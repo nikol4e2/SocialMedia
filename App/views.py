9c537afda5b9392158ebdbff0d760a163dde3dae
Nikola Arsov <arsovnikola1@gmail.com>
2023-09-16 20:41:39 +0200

    added login and logout functionality

--- App/views.py
+++ App/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from App.models import Profile
 
 
 # Create your views here.
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
 
 def signup(request):
-
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
@@ -21,24 +21,46 @@ def signup(request):
 
         if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request,"User with that email already exists!")
+                messages.info(request, "User with that email already exists!")
                 return redirect('signup')
             elif User.objects.filter(username=username).exists():
-                messages.info(request,"Username is already taken!")
-                return  redirect('signup')
+                messages.info(request, "Username is already taken!")
+                return redirect('signup')
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                #log user in and redirect to settings page
+                # log user in and redirect to settings page
 
-                #create a Profile object for the new user
+                # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
                 return redirect('login')
         else:
-            messages.info(request,"Passwords do not match!")
+            messages.info(request, "Passwords do not match!")
 
     else:
         return render(request, 'signup.html')
+
+
+def signin(reqeust):
+    if reqeust.method == 'POST':
+        username = reqeust.POST['username']
+        password = reqeust.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(reqeust, user)
+            return redirect('/')
+        else:
+            messages.info(reqeust, "Credentials are invalid")
+            return redirect('signin')
+
+    return render(reqeust, 'signin.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
--- SocialMedia/urls.py
+++ SocialMedia/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('App.urls'))
+    path('', include('App.urls')),
+
 
 ]
 
